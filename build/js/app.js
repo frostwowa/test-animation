a61942d9c31f243b9fbc546400424728a4cabea3
Moroz Volodymyr <frostwowa@gmail.com>
2017-12-18 08:36:59 +0200

    option 1

--- build/js/app.js
+++ build/js/app.js
@@ -2412,6 +2412,62 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     }();
 }).call(this);
 
+(function () {
+    $('.js-price-btn').hover(function () {
+        $(this).parents('.js-price').find('.js-price-head').css('background-color', '#2E2EFE');
+    }, function () {
+        $(this).parents('.js-price').find('.js-price-head').css('background-color', '#5b6ceb');
+    });
+})();
+
+// tabs
+(function () {
+    var tabs = $('.js-tabs');
+    tabs.each(function () {
+        var thisTabs = $(this),
+            nav = thisTabs.find('.js-tabs-link'),
+            item = thisTabs.find('.js-tabs-item');
+        nav.on('click', function () {
+            var thisNav = $(this),
+                indexNav = thisNav.index();
+            nav.removeClass('active');
+            thisNav.addClass('active');
+            item.hide();
+            item.eq(indexNav).fadeIn();
+
+            if (thisNav.hasClass('js-tabs-link-map')) {
+                google.maps.event.trigger(map, "resize");
+            }
+
+            return false;
+        });
+    });
+})();
+
+// tabs
+(function () {
+    var tabs = $('.js-tabs');
+    tabs.each(function () {
+        var thisTabs = $(this),
+            nav = thisTabs.find('.js-tabs-link'),
+            item = thisTabs.find('.js-tabs-item');
+        nav.on('click', function () {
+            var thisNav = $(this),
+                indexNav = thisNav.index();
+            nav.removeClass('active');
+            thisNav.addClass('active');
+            item.hide();
+            item.eq(indexNav).fadeIn();
+
+            if (thisNav.hasClass('js-tabs-link-map')) {
+                google.maps.event.trigger(map, "resize");
+            }
+
+            return false;
+        });
+    });
+})();
+
 // wow
 (function () {
     var posScroll = $(window).scrollTop();
@@ -2457,4 +2513,87 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
             popup.hide();
         }, 300);
     }
+})();
+
+$(function () {
+
+    var $burger = $('.burger');
+    var $menu = $('.js-menu');
+    var $bars = $('.burger-svg__bars');
+    var $bar = $('.burger-svg__bar');
+    var $bar1 = $('.burger-svg__bar-1');
+    var $bar2 = $('.burger-svg__bar-2');
+    var $bar3 = $('.burger-svg__bar-3');
+    var isChangingState = false;
+    var isOpen = false;
+    var burgerTL = new TimelineMax();
+
+    function burgerOver() {
+
+        if (!isChangingState) {
+            burgerTL.clear();
+            if (!isOpen) {
+                burgerTL.to($bar1, 0.5, { y: -2, ease: Elastic.easeOut }).to($bar2, 0.5, { scaleX: 0.6, ease: Elastic.easeOut, transformOrigin: "50% 50%" }, "-=0.5").to($bar3, 0.5, { y: 2, ease: Elastic.easeOut }, "-=0.5");
+            } else {
+                burgerTL.to($bar1, 0.5, { scaleX: 1.2, ease: Elastic.easeOut }).to($bar3, 0.5, { scaleX: 1.2, ease: Elastic.easeOut }, "-=0.5");
+            }
+        }
+    }
+
+    function burgerOut() {
+        if (!isChangingState) {
+            burgerTL.clear();
+            if (!isOpen) {
+                burgerTL.to($bar1, 0.5, { y: 0, ease: Elastic.easeOut }).to($bar2, 0.5, { scaleX: 1, ease: Elastic.easeOut, transformOrigin: "50% 50%" }, "-=0.5").to($bar3, 0.5, { y: 0, ease: Elastic.easeOut }, "-=0.5");
+            } else {
+                burgerTL.to($bar1, 0.5, { scaleX: 1, ease: Elastic.easeOut }).to($bar3, 0.5, { scaleX: 1, ease: Elastic.easeOut }, "-=0.5");
+            }
+        }
+    }
+
+    function showCloseBurger() {
+        burgerTL.clear();
+        burgerTL.to($bar1, 0.3, { y: 6, ease: Power4.easeIn }).to($bar2, 0.3, { scaleX: 1, ease: Power4.easeIn }, "-=0.3").to($bar3, 0.3, { y: -6, ease: Power4.easeIn }, "-=0.3").to($bar1, 0.5, { rotation: 45, ease: Elastic.easeOut, transformOrigin: "50% 50%" }).set($bar2, { opacity: 0, immediateRender: false }, "-=0.5").to($bar3, 0.5, { rotation: -45, ease: Elastic.easeOut, transformOrigin: "50% 50%", onComplete: function onComplete() {
+                isChangingState = false;isOpen = true;
+            } }, "-=0.5");
+    }
+
+    function showOpenBurger() {
+        burgerTL.clear();
+        burgerTL.to($bar1, 0.3, { scaleX: 0, ease: Back.easeIn }).to($bar3, 0.3, { scaleX: 0, ease: Back.easeIn }, "-=0.3").set($bar1, { rotation: 0, y: 0 }).set($bar2, { scaleX: 0, opacity: 1 }).set($bar3, { rotation: 0, y: 0 }).to($bar2, 0.5, { scaleX: 1, ease: Elastic.easeOut }).to($bar1, 0.5, { scaleX: 1, ease: Elastic.easeOut }, "-=0.4").to($bar3, 0.5, { scaleX: 1, ease: Elastic.easeOut, onComplete: function onComplete() {
+                isChangingState = false;isOpen = false;
+            } }, "-=0.5");
+    }
+
+    $burger.on('click', function (e) {
+
+        if (!isChangingState) {
+            isChangingState = true;
+
+            if (!isOpen) {
+                showCloseBurger();
+            } else {
+                showOpenBurger();
+            }
+        }
+    });
+
+    $burger.hover(burgerOver, burgerOut);
+
+    burger.on('click', function () {
+        burger.toggleClass('active');
+        menu.toggleClass('visible');
+    });
+});
+
+console.clear();
+
+// menu
+(function () {
+    var burger = $('.burger'),
+        menu = $('.js-menu');
+    burger.on('click', function () {
+        burger.toggleClass('active');
+        menu.toggleClass('visible');
+    });
 })();
